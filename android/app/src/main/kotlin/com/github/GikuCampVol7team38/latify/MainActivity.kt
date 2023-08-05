cf3c3e20a00984f9faf6bf3a0d69c3121089b08c
k-taro56 <121674121+k-taro56@users.noreply.github.com>
2023-08-05 11:48:25 +0900

    add setAlarm function

--- android/app/src/main/kotlin/com/github/GikuCampVol7team38/latify/MainActivity.kt
+++ android/app/src/main/kotlin/com/github/GikuCampVol7team38/latify/MainActivity.kt
@@ -1,6 +1,39 @@
 package com.github.GikuCampVol7team38.latify
 
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.provider.AlarmClock
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
+    private val CHANNEL = "com.github.GeekCampVol7team38.latify/alarm"
+
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
+                call, result ->
+            if (call.method == "setAlarm") {
+                val hour = call.argument<Int>("hour") ?: 0
+                val minute = call.argument<Int>("minute") ?: 0
+                setAlarm(this, hour, minute)
+                result.success("Alarm Set Successfully")
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
+
+    private fun setAlarm(context: Context, hour: Int, minute: Int) {
+        val intent = Intent(AlarmClock.ACTION_SET_ALARM)
+        intent.putExtra(AlarmClock.EXTRA_HOUR, hour)
+        intent.putExtra(AlarmClock.EXTRA_MINUTES, minute)
+        intent.putExtra(AlarmClock.EXTRA_SKIP_UI, true)
+        context.startActivity(intent)
+    }
 }
