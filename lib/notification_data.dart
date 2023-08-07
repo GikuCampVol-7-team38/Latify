42783c969e43ee15453e6df42f6a6e064c40e892
k-taro56 <121674121+k-taro56@users.noreply.github.com>
2023-08-08 00:34:55 +0900

    add NotificationData class

---
+++ lib/notification_data.dart
@@ -0,0 +1,35 @@
+import 'marshallingData.dart';
+
+class NotificationData {
+  final StatusBarNotification statusBarNotification;
+
+  NotificationData(this.statusBarNotification);
+
+  Map<String, Object?>? toMap() {
+    return {
+    'statusBarNotification' : statusBarNotification.toMap(),
+    };
+  }
+
+  static NotificationData? fromDynamic(dynamic map) {
+    if (map is! Map<dynamic, dynamic>) {
+      return null;
+    }
+    final statusBarNotification = StatusBarNotification.fromDynamic(map['statusBarNotification']);
+    if (statusBarNotification == null) {
+      return null;
+    }
+    return NotificationData(statusBarNotification);
+  }
+
+  static NotificationData? fromMap(Map<Object?, Object?>? map) {
+    if (map == null) {
+      return null;
+    }
+    final statusBarNotification = StatusBarNotification.fromMap(map['statusBarNotification'] is Map<Object?, Object?> ? map['statusBarNotification'] as Map<Object?, Object?> : null);
+    if (statusBarNotification == null) {
+      return null;
+    }
+    return NotificationData(statusBarNotification);
+  }
+}
