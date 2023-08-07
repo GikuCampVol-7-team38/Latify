1a1356214add86185c1ce7826531368640984449
k-taro56 <121674121+k-taro56@users.noreply.github.com>
2023-08-08 00:46:31 +0900

    make private

--- lib/alarm.dart
+++ lib/alarm.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/services.dart';
 
 class AlarmService {
-  static const platform = MethodChannel('com.github.GeekCampVol7team38.latify/alarm');
+  static const _platform = MethodChannel('com.github.GeekCampVol7team38.latify/alarm');
 
   static Future<void> setAlarm(int hour, int minute) async {
     try {
-      await platform.invokeMethod('setAlarm', {'hour': hour, 'minute': minute});
+      await _platform.invokeMethod('setAlarm', {'hour': hour, 'minute': minute});
     } on PlatformException catch (_) {
     }
   }
