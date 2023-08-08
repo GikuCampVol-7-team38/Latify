3bf74c24387fef89ba228226d305dfcb776c17aa
k-taro56 <121674121+k-taro56@users.noreply.github.com>
2023-08-09 04:20:19 +0900

    add NotionData class

---
+++ android/app/src/main/kotlin/com/github/GikuCampVol7team38/latify/NotionData.kt
@@ -0,0 +1,87 @@
+package com.github.GikuCampVol7team38.latify
+
+import android.content.Context
+import java.io.File
+
+class NotionData {
+
+    companion object {
+        fun load(context: Context): NotionData {
+            val file = File(context.filesDir, "NotionData")
+            if (file.exists()) {
+                val bytes = file.readBytes()
+                val map = MyMessagePack.unpack(bytes)
+                return NotionData().apply {
+                    decrypt(EncryptedData.fromMap(map))
+                }               
+            } else {
+                return NotionData()
+            }
+        }
+    }
+
+    fun save(context: Context) {
+        val bytes = MyMessagePack.pack(encrypt().toMap())
+        val file = File(context.filesDir, "NotionData")
+        file.writeBytes(bytes)
+    }
+
+    var ApiKey: String = "";
+    val databaseIDMap: MutableMap<String, String> = mutableMapOf<String, String>();
+
+    private fun encrypt(): EncryptedData {
+        val dataEncryptor = DataEncryptor()
+        val encryptedDatabaseIDMap = mutableMapOf<String, MutableMap<String, ByteArray>>()
+        for ((key, value) in databaseIDMap) {
+            val map = mutableMapOf<String, ByteArray>()
+            dataEncryptor.encrypt(value).let { (encrypted, iv) ->
+                map["encryptedDatabaseID"] = encrypted
+                map["iv"] = iv
+            }
+            encryptedDatabaseIDMap[key] = map
+        }
+        dataEncryptor.encrypt(ApiKey).let { (encrypted, iv) ->
+        return EncryptedData(encrypted, iv, encryptedDatabaseIDMap)
+        }
+    }
+
+    private fun decrypt(encryptedData: EncryptedData) {
+        val dataEncryptor = DataEncryptor()
+        dataEncryptor.decrypt(encryptedData.encryptedApiKey, encryptedData.iv).let {
+            ApiKey = it
+        }
+        for ((key, value) in encryptedData.encryptedDatabaseIDMap) {
+            val encryptedDatabaseID = value["encryptedDatabaseID"]
+            val iv = value["iv"]
+            if (encryptedDatabaseID == null || iv == null) {
+                continue
+            }
+            dataEncryptor.decrypt(encryptedDatabaseID, iv).let {
+                databaseIDMap[key] = it
+            }
+        }
+    }
+
+    private class EncryptedData (
+        val encryptedApiKey: ByteArray, 
+        val iv: ByteArray, 
+        val encryptedDatabaseIDMap: MutableMap<String, MutableMap<String, ByteArray>>
+        ) {
+        companion object {
+            fun fromMap(map: Map<String, Any?>): EncryptedData {
+                return EncryptedData(map["encryptedApiKey"] as ByteArray,
+                 map["iv"] as ByteArray,
+                 map["encryptedDatabaseIDMap"] as MutableMap<String, MutableMap<String, ByteArray>>
+                )
+            }
+        }
+
+        fun toMap(): Map<String, Any?> {
+            return mapOf(
+                    "encryptedApiKey" to encryptedApiKey,
+                    "iv" to iv,
+                    "encryptedDatabaseIDMap" to encryptedDatabaseIDMap
+            )
+        }
+    }
+}
