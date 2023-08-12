0f04b37d5a1e99e61f891a0f91b029273caf4304
k-taro56 <121674121+k-taro56@users.noreply.github.com>
2023-08-12 19:24:28 +0900

    set default JSON

--- android/app/src/main/kotlin/com/github/GikuCampVol7team38/latify/NotionTemplate.kt
+++ android/app/src/main/kotlin/com/github/GikuCampVol7team38/latify/NotionTemplate.kt
@@ -7,37 +7,60 @@ class  NotionTemplate {
 
     companion object {
         fun getRecieved(context: Context): String {
+            val default =
+                "\"properties\":{\"title\":{\"title\":[{\"text\":{\"content\":\"\${notificationContent}\"}}]}," +
+                        "\"タグ\":{\"type\":\"multi_select\",\"multi_select\":[{\"name\":\"\${appName}\"}]}}," +
+                        "\"children\":[{\"object\":\"block\",\"type\":\"paragraph\",\"paragraph\":" +
+                        "{\"text\":[{\"type\":\"text\",\"text\":{\"content\":\"Notification Date: \${postDate}\\n\"}}]}}]"
+
             val file = File(context.filesDir, "Recieved")
+            if (!file.exists()) {
+                return default
+            }
             val str = EncryptedStorage.readString(file)
             if (str.isEmpty()) {
-                return ""
+                return default
             }
             return str
         }
 
+        private val defaultButtonPressedTemplate = "\"properties\":{\"title\":{\"title\":[{\"text\":{\"content\":\"\${buttonName}\"}}]}," +
+                "\"タグ\":{\"type\":\"multi_select\",\"multi_select\":[{\"name\":\"\${appName}\"}]}}," +
+                "\"children\":[{\"object\":\"block\",\"type\":\"paragraph\",\"paragraph\":" +
+                "{\"text\":[{\"type\":\"text\",\"text\":{\"content\":\"Notification Date: \${postDate}\\n\\n\${notificationContent}\\n\"}}]}}]"
+
         fun getLeftButton(context: Context): String {
             val file = File(context.filesDir, "LeftButton")
+            if (!file.exists()) {
+                return defaultButtonPressedTemplate
+            }
             val str = EncryptedStorage.readString(file)
             if (str.isEmpty()) {
-                return ""
+                return defaultButtonPressedTemplate
             }
             return str
         }
 
         fun getMiddleButton(context: Context): String {
             val file = File(context.filesDir, "MiddleButton")
+            if (!file.exists()) {
+                return defaultButtonPressedTemplate
+            }
             val str = EncryptedStorage.readString(file)
             if (str.isEmpty()) {
-                return ""
+                return defaultButtonPressedTemplate
             }
             return str
         }
 
         fun getRightButton(context: Context): String {
             val file = File(context.filesDir, "RightButton")
+            if (!file.exists()) {
+                return defaultButtonPressedTemplate
+            }
             val str = EncryptedStorage.readString(file)
             if (str.isEmpty()) {
-                return ""
+                return defaultButtonPressedTemplate
             }
             return str
         }
