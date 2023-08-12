b19997490bc8d05b4c86d4a17443bad74a7be23b
k-taro56 <121674121+k-taro56@users.noreply.github.com>
2023-08-12 17:28:37 +0900

    add debug message when send to notion

--- android/app/src/main/kotlin/com/github/GikuCampVol7team38/latify/NotionTerminal.kt
+++ android/app/src/main/kotlin/com/github/GikuCampVol7team38/latify/NotionTerminal.kt
@@ -37,6 +37,8 @@ class NotionTerminal{
             val apiKey = notionData.ApiKey
             val databaseID = notionData.databaseIDMap[databaseKey]
 
+            val jhon ="\"properties\": {\"title\": {\"title\": [{\"text\": {\"content\": \"新しいページのタイトル\"}}]}}, \"children\": [{\"object\": \"block\", \"type\": \"paragraph\", \"paragraph\": {\"text\": [{\"type\": \"text\", \"text\": {\"content\": \"これは新しく追加されたテキストブロックの内容です。\"}}]}}]"
+            
             val url = URL("https://api.notion.com/v1/pages")
             return with(url.openConnection() as HttpURLConnection) {
                 requestMethod = "POST"
@@ -48,8 +50,17 @@ class NotionTerminal{
                 setRequestProperty("Notion-Version", "2021-08-16")
 
                 OutputStreamWriter(outputStream, Charsets.UTF_8).use {
+                    println("{\"parent\":{\"database_id\":\"$databaseID\"},$json}")
                     it.write("{\"parent\":{\"database_id\":\"$databaseID\"},$json}")
                     it.flush()
+                }
+
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    val responseContent = inputStream.bufferedReader().use { it.readText() }
+                    println("Response: $responseContent")
+                } else {
+                    val errorResponse = errorStream.bufferedReader().use { it.readText() }
+                    println("Error Response: $errorResponse")
                 }
 
                 responseCode == HttpURLConnection.HTTP_OK
